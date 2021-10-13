b1242013e74499d3202d0e3f2803d96abe14d2f1
Elienock Mulumba <lubayaelienock@gmail.com>
2021-10-13 07:39:57 +0200

    9th commit

--- js/script.js
+++ js/script.js
@@ -46,15 +46,16 @@ var swiper = new Swiper(".home-slider", {
   loop:true,
 });
 
+
 window.onscroll = () =>{
 
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 
   if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
+    document.querySelector('#chat').classList.add('active');
   }else{
-    document.querySelector('#scroll-top').classList.remove('active');
+    document.querySelector('#chat').classList.remove('active');
   }
 
 }
@@ -68,3 +69,4 @@ function fadeOut(){
 }
 
 window.onload = fadeOut;
+
